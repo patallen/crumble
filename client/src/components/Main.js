83d532cf1aea0d6c18daeb5430c34d76845bffb1
Patrick Allen <prallen90@gmail.com>
2016-04-17 13:34:11 -0400

    Add basic layout

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 export default class Main extends React.Component {
-
 	render() {
-		var counter = 8;
-		return <h1>Hello, It's me...{counter}</h1>
+		return (
+			<div className="main col-md-offset-3 col-md-6">
+				<div className="row padding-top">
+					<div className="col-xs-10">
+						<input className="form-control" />
+					</div>
+					<div className="col-xs-2">
+						<button className="btn btn-success btn-block">Go!</button>
+					</div>
+				</div>
+				<div className="row padding-top">
+					<div className="col-xs-12">
+						<textarea className="form-control" rows="20"></textarea>
+					</div>
+				</div>
+				<div className="row padding-top">
+					<div className="col-xs-12">
+						<button className="btn btn-success btn-block">Update</button>
+					</div>
+				</div>
+			</div>
+		)
 	}
 }
