2465a13497084948451e2510554e21d48cd9ad3a
Patrick Allen <prallen90@gmail.com>
2016-04-21 17:58:36 -0400

    Add after_request CORS '*' origin stuff

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -10,3 +10,10 @@ db = SQLAlchemy(app)
 
 from app.views import public
 app.register_blueprint(public, url_prefix='/public')
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
