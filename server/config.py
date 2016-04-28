64ebc7623d34828a5a30a348ba9f5ed3ee3a7fb1
Patrick Allen <prallen90@gmail.com>
2016-04-27 21:45:59 -0400

    Fix some small cipher problems

--- server/config.py
+++ server/config.py
@@ -4,3 +4,5 @@ home_dir = os.path.dirname(os.path.abspath(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:////{}/crumble.db'.format(home_dir)
 DEBUG = True
+
+SECRET_KEY = 'change-this-in-p'
--- server/lib/encrypt.py
+++ server/lib/encrypt.py
@@ -1,7 +1,9 @@
-from Crypto.Cypher import AES
+from Crypto.Cipher import AES
+from Crypto import Random
 from app import app
 
-encryptor = AES.new(app.config.get('SECRET_KEY'))
+iv = Random.new().read(AES.block_size)
+encryptor = AES.new(app.config.get('SECRET_KEY'), AES.MODE_CBC, iv)
 
 
 def encrypt(string):
