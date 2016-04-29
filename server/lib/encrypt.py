e4f6c4e848f977d5304dd917bab5324a23aa84b4
Patrick Allen <prallen90@gmail.com>
2016-04-28 23:00:07 -0400

    Add pad helper function for AES block size

--- server/lib/encrypt.py
+++ server/lib/encrypt.py
@@ -6,8 +6,20 @@ iv = Random.new().read(AES.block_size)
 encryptor = AES.new(app.config.get('SECRET_KEY'), AES.MODE_CBC, iv)
 
 
+def pad(string, block_size=16):
+    to_pad = len(string) % block_size
+
+    if to_pad >= 0:
+        digits = 1
+    if to_pad > 9:
+        digits = 2
+
+    zeros = block_size + to_pad - digits
+    return "{0}{1}".format(string, ("{}{}".format('0'*zeros, str(to_pad))))
+
+
 def encrypt(string):
-    return encryptor.encrypt(string)
+    return encryptor.encrypt(pad(string))
 
 
 def decrypt(string):
