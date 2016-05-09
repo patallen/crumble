d0c54dfb63c2a570b4551d5a93201f6c604edd21
Patrick Allen <prallen90@gmail.com>
2016-05-08 21:33:57 -0400

    Add manage command for creating admin user

--- server/manage.py
+++ server/manage.py
@@ -1,5 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import prompt, prompt_pass
 
 from app import app, db
 
@@ -9,6 +10,20 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 from app import models
+
+
+@manager.command
+def create_admin():
+    email = prompt("Enter an email for this Administrator")
+    pass1 = prompt_pass("Enter password")
+    pass2 = prompt_pass("Re-enter password")
+
+    if pass1 == pass2:
+        admin = models.Admin(email=email)
+        admin.password = pass1
+        admin.save()
+    else:
+        print "Passwords do not match. Try again."
 
 
 if __name__ == '__main__':
